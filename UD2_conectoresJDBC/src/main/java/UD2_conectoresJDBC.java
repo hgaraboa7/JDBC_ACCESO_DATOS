dfc1d383d2f2b65167a8eafc21d0a815ae2f2435
Hgaraboa7 <153545940+hgaraboa7@users.noreply.github.com>
2024-10-22 22:10:55 +0200

    Add files via upload

---
+++ UD2_conectoresJDBC/src/main/java/UD2_conectoresJDBC.java
@@ -0,0 +1,25 @@
+
+import controlador.ControladorEjercicio1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author hector.garaboacasas
+ */
+public class UD2_conectoresJDBC {
+
+    public static void main(String[] args) {
+        
+        //1.- inicio el controlador
+        ControladorEjercicio1.iniciar();
+        
+        
+        
+        
+    }
+    
+    
+}
---
+++ UD2_conectoresJDBC/src/main/java/controlador/ControladorEjercicio1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import vista.Ejercicio1;
+
+/**
+ *
+ * @author hector.garaboacasas
+ */
+public class ControladorEjercicio1 {
+
+    public static Ejercicio1 ventana= new Ejercicio1();
+    
+    public static void iniciar() {
+        
+        ventana.setVisible(true);
+        
+        ventana.setLocationRelativeTo(null);
+        
+        }
+    
+}
